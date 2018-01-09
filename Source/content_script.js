d536e0ea8f615207de0493814c3b7b8361a0e299
Techjar <tecknojar@gmail.com>
2018-01-08 20:29:21 -0500

    Ignore contenteditable elements to prevent issues

--- Source/content_script.js
+++ Source/content_script.js
@@ -32,7 +32,7 @@ function walk(node)
 			break;
 
 		case 3: // Text node
-            if(node.parentElement != null && node.parentElement.tagName.toLowerCase() != "script" && node.parentElement.tagName.toLowerCase() != "style" && node.parentElement.tagName.toLowerCase() != "textarea") {
+            if(node.parentElement != null && node.parentElement.tagName.toLowerCase() != "script" && node.parentElement.tagName.toLowerCase() != "style" && node.parentElement.tagName.toLowerCase() != "textarea" && node.parentElement.contentEditable != "true") {
                 handleText(node);
             }
 			break;
